d95dc248bedb058bfc2709c1e4b8dcee5b532949
Alex Taker <A.Taker@gmail.com>
2015-06-06 11:57:42 -0400

    issue 1, format JSON

--- realestate.js
+++ realestate.js
@@ -1,34 +1,39 @@
 var fileref=document.createElement('script')
-    fileref.setAttribute("type","text/javascript")
-    fileref.setAttribute("src", "https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js")
-    fileref.onload = scrapePage;
+	fileref.setAttribute("type","text/javascript")
+	fileref.setAttribute("src", "https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js")
+	fileref.onload = scrapePage;
 	document.getElementsByTagName("head")[0].appendChild(fileref)
 
 
+
 function scrapePage(){
-	var e = "Address \t Description \t Price \n";
+
+	var url = window.location.href
+	url = url.slice(0,url.lastIndexOf("/")+1);
+	var data = [];
 	var tbody = $("tbody")[6];
+	$(tbody).children("tr")[0].remove();
+
 	$(tbody).find("tr").each(function(tr){
-		var count = 0;
+		var count = 0, entry={};
 		$(this).find("td").each(function(td){
 			td = this;
 			if(count == 9){
-				e += stripTabs($(td).children("a").text());
-				e += " \t "
+				entry.address = stripTabs($(td).children("a").text());
+				entry.link = url + stripTabs($(td).children("a").attr("href"));
 			}
 			if(count == 11){
-				e += stripTabs($(td).children("a").text());
-				e += " \t "
+				entry.description = stripTabs($(td).children("a").text());
 			}
 			if(count == 13){
-				e += stripTabs($(td).text());
-				e += " \n "
+				entry.price = stripTabs($(td).text());
 			}
 			count++;
 		});
+		data.push(entry)
 
 	});
-	console.log(e);
+	console.log(data);
 }
 
 
