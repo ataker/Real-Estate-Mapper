d7d33e3c1beb5cd0f2019fe6f578811de4dc2e5a
Alex Taker <A.Taker@gmail.com>
2015-06-06 11:28:36 -0400

    issues 2 and 3

--- realestate.js
+++ realestate.js
@@ -1,41 +1,37 @@
 var fileref=document.createElement('script')
-        fileref.setAttribute("type","text/javascript")
-        fileref.setAttribute("src", "https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js")
-		document.getElementsByTagName("head")[0].appendChild(fileref)
+    fileref.setAttribute("type","text/javascript")
+    fileref.setAttribute("src", "https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js")
+    fileref.onload = scrapePage;
+	document.getElementsByTagName("head")[0].appendChild(fileref)
 
-var e = "Address \t Description \t Price \n";
-var tbody = $(".tbody");
-$(tbody).find("tr").each(function(tr){
-	var count = 0;
-	$(this).find("td").each(function(td){
-		td = this;
-		if(count == 9){
-			e += stripTabs($(td).children("a").text());
-	//		e += " \n";
-	//	}
-			e += " \t "
-		}
-		if(count == 11){
-			e += stripTabs($(td).children("a").text());
-			e += " \t "
-		}
-		if(count == 13){
-			e += stripTabs($(td).text());
-			e += " \n "
-		}
-		count++;
+
+function scrapePage(){
+	var e = "Address \t Description \t Price \n";
+	var tbody = $("tbody")[6];
+	$(tbody).find("tr").each(function(tr){
+		var count = 0;
+		$(this).find("td").each(function(td){
+			td = this;
+			if(count == 9){
+				e += stripTabs($(td).children("a").text());
+				e += " \t "
+			}
+			if(count == 11){
+				e += stripTabs($(td).children("a").text());
+				e += " \t "
+			}
+			if(count == 13){
+				e += stripTabs($(td).text());
+				e += " \n "
+			}
+			count++;
+		});
+
 	});
-
-});
 	console.log(e);
+}
 
-	function stripTabs(str){
-		return str.replace( new RegExp("\t", 'g'),"").replace( new RegExp("\n", 'g')," ").replace( new RegExp("&nbsp", 'g')," ");
-	}
 
-/*
-	var fileref=document.createElement('script')
-        fileref.setAttribute("type","text/javascript")
-        fileref.setAttribute("src", "https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js")
-		document.getElementsByTagName("head")[0].appendChild(fileref)
-        */
+function stripTabs(str){
+	return str.replace( new RegExp("\t", 'g'),"").replace( new RegExp("\n", 'g')," ").replace( new RegExp("&nbsp", 'g')," ");
+}
